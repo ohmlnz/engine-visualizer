47c04b6b3b6ffdfb47ca2ac311b2f3204943ae06
OthmanLan <othman.lanizi@gmail.com>
2019-07-15 01:05:19 -0400

    Hide options and handle multiple entities as well as custom maps.

--- src/App.js
+++ src/App.js
@@ -1,22 +1,31 @@
-import React, { Component } from 'react'
-import Game from './engine/game.js'
-import PhysicsEntity from './engine/core/model.js'
-import Engine from './engine/core/engine.js'
-import Canvas from './components/canvas'
-import Options from './components/options'
+import React, { Component } from 'react';
+import Game from './engine/game.js';
+import PhysicsEntity from './engine/core/model.js';
+import Engine from './engine/core/engine.js';
+import Canvas from './components/canvas';
+import Options from './components/options';
 import Controls from './engine/controller/controls.js';
+import { levelOne, randomLevel } from './engine/maps/level-1.js';
 
 class App extends Component {
+  createEntities = level => {
+    let entities = [];
+    Object.entries(level).forEach(e => {
+      entities.push(new PhysicsEntity(...e[1].dimensions, e[1].color));
+    });
+    return entities;
+  };
+
   render() {
-    let player = new PhysicsEntity(100, 200, 'black');
-    let entity = new PhysicsEntity(200, 200, 'blue');
-    let controls = new Controls()
-    let engine = new Engine();
-    let game = new Game(player, entity, engine, controls)
+    const player = new PhysicsEntity(100, 200, 50, 50, 'black');
+    const entities = this.createEntities(randomLevel());
+    const controls = new Controls();
+    const engine = new Engine();
+    const game = new Game(player, entities, engine, controls);
     return (
       <div className="App">
         <Canvas game={game} />
-        <Options game={game} />
+        {/* <Options game={game} /> */}
       </div>
     );
   }
--- src/components/options.js
+++ src/components/options.js
@@ -1,7 +1,7 @@
-import React, { Component } from 'react'
-import inputs from './inputs.json'
-import _ from 'lodash'
-import './style.css'
+import React, { Component } from 'react';
+import inputs from './inputs.json';
+import _ from 'lodash';
+import './style.css';
 
 class Options extends Component {
   state = {
@@ -9,33 +9,35 @@ class Options extends Component {
     ax: 0.03,
     x: 100,
     y: 100
-  }
+  };
 
   changeValue = (e, obj) => {
-    let value = e.target.value
-    let label = e.target.getAttribute('label')
+    let value = e.target.value;
+    let label = e.target.getAttribute('label');
     this.setState({ [label]: +value }, () => {
-      obj[label] = +value
-    })
-  }
+      obj[label] = +value;
+    });
+  };
 
   render() {
     return (
-      <ul className='options'>
-        { inputs.map((field, i) => (
-            <li key={i}>
-              <span>{field.label}</span>
-              <input 
-                type='number' 
-                label={field.label}
-                onChange={(e) => this.changeValue(e, _.get(this.props, field.object))}
-                value={this.state[field.label]}
-              />
-            </li>
-        )) }
+      <ul className="options">
+        {/* {inputs.map((field, i) => (
+          <li key={i}>
+            <span>{field.label}</span>
+            <input
+              type="number"
+              label={field.label}
+              onChange={e =>
+                this.changeValue(e, _.get(this.props, field.object))
+              }
+              value={this.state[field.label]}
+            />
+          </li>
+        ))} */}
       </ul>
-    )
+    );
   }
-} 
+}
 
-export default Options
+export default Options;
