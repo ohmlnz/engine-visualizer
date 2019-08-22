8bd6994179383714ccead133f65ddb9fa6ee2fcb
OthmanLan <othman.lanizi@gmail.com>
2019-08-22 16:47:16 +0200

    Load random background and add ability to switch.

--- src/App.js
+++ src/App.js
@@ -5,12 +5,27 @@ import Engine from './engine/core/engine.js';
 import Canvas from './components/canvas';
 import Options from './components/options';
 import Controls from './engine/controller/controls.js';
-import { levelOne, randomLevel } from './engine/maps/level-1.js';
+import { levelOne } from './engine/maps/level-1.js';
 import { constants } from './engine/constants.js';
 import { spriteData } from './engine/sprites/businessman/businessman';
+import { displacePoints, generateBackground } from './engine/maps/helpers.js';
 
 class App extends Component {
   state = { constants };
+
+  componentDidMount() {
+    this.changeBackground();
+  }
+
+  changeBackground = () => {
+    // Generate random layers
+    const layer1 = displacePoints([-10, 250], [1010, 250], 1.3, 225, 10);
+    const layer2 = displacePoints([-10, 250], [1010, 250], 1.2, 125, 10);
+
+    this.setState({
+      background: generateBackground([layer1, layer2]).src
+    });
+  };
 
   createEntities(level) {
     let entities = [];
@@ -53,7 +68,11 @@ class App extends Component {
           [label]: +value
         }
       },
-      () => (game.player[label] = +value)
+      () => {
+        if (game && game.player[label]) {
+          game.player[label] = +value;
+        }
+      }
     );
   };
 
@@ -69,8 +88,11 @@ class App extends Component {
 
     return (
       <div className="App">
-        <Canvas game={game} state={constants} />
+        <Canvas game={game} state={this.state} />
         <Options game={game} state={constants} changeValue={this.changeValue} />
+        <div className="background-picker" onClick={this.changeBackground}>
+          Change background
+        </div>
       </div>
     );
   }
